d30547adef3a408b21e23323a800e319f7ed5c69
rsgc-ser-jon <jser@rsgc.on.ca>
2016-03-30 12:07:45 -0400

    Updated Database Layout

--- db/lunches-init.sql
+++ db/lunches-init.sql
@@ -5,22 +5,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mrgogor3_RRUSR
+-- Schema mrgogor3_RR
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mrgogor3_RRUSR` ;
+DROP SCHEMA IF EXISTS `mrgogor3_RR` ;
 
 -- -----------------------------------------------------
--- Schema mrgogor3_RRUSR
+-- Schema mrgogor3_RR
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mrgogor3_RRUSR` DEFAULT CHARACTER SET utf8 ;
-USE `mrgogor3_RRUSR` ;
+CREATE SCHEMA IF NOT EXISTS `mrgogor3_RR` DEFAULT CHARACTER SET utf8 ;
+USE `mrgogor3_RR` ;
 
 -- -----------------------------------------------------
--- Table `mrgogor3_RRUSR`.`user`
+-- Table `mrgogor3_RR`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mrgogor3_RRUSR`.`user` ;
+DROP TABLE IF EXISTS `mrgogor3_RR`.`user` ;
 
-CREATE TABLE IF NOT EXISTS `mrgogor3_RRUSR`.`user` (
+CREATE TABLE IF NOT EXISTS `mrgogor3_RR`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `pw` VARCHAR(255) NOT NULL,
   `username` VARCHAR(45) NOT NULL,
@@ -31,33 +31,30 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mrgogor3_RRUSR`.`restaurant`
+-- Table `mrgogor3_RR`.`restaurant`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mrgogor3_RRUSR`.`restaurant` ;
+DROP TABLE IF EXISTS `mrgogor3_RR`.`restaurant` ;
 
-CREATE TABLE IF NOT EXISTS `mrgogor3_RRUSR`.`restaurant` (
+CREATE TABLE IF NOT EXISTS `mrgogor3_RR`.`restaurant` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `walkingdistance` INT NULL,
-  `adress` VARCHAR(45) NOT NULL,
-  `expense` INT NULL,
-  `description` VARCHAR(300) NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `timeToWalk` INT NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `description` TEXT(2000) NOT NULL,
   `menu` VARCHAR(1000) NULL,
+  `imageName` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mrgogor3_RRUSR`.`rating`
+-- Table `mrgogor3_RR`.`rating`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mrgogor3_RRUSR`.`rating` ;
+DROP TABLE IF EXISTS `mrgogor3_RR`.`rating` ;
 
-CREATE TABLE IF NOT EXISTS `mrgogor3_RRUSR`.`rating` (
+CREATE TABLE IF NOT EXISTS `mrgogor3_RR`.`rating` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `servTime` INT NULL,
-  `date` DATETIME NOT NULL,
-  `starRating` INT NULL,
-  `comment` VARCHAR(300) NULL,
+  `starRating` INT NOT NULL,
   `user_id` INT NOT NULL,
   `restaurant_id` INT NOT NULL,
   PRIMARY KEY (`id`),
@@ -65,12 +62,12 @@ CREATE TABLE IF NOT EXISTS `mrgogor3_RRUSR`.`rating` (
   INDEX `fk_rating_restaurant1_idx` (`restaurant_id` ASC),
   CONSTRAINT `fk_rating_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `mrgogor3_RRUSR`.`user` (`id`)
+    REFERENCES `mrgogor3_RR`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_rating_restaurant1`
     FOREIGN KEY (`restaurant_id`)
-    REFERENCES `mrgogor3_RRUSR`.`restaurant` (`id`)
+    REFERENCES `mrgogor3_RR`.`restaurant` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
